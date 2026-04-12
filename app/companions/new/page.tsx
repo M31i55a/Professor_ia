2e26eab911bd67a418539c9a02bacfa51d91591e
M31i55a <melysgioda34@gmail.com>
2026-04-12 19:20:40 +0100

    created the agent creation form and installed Clerk

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import CompanionForm from "@/components/CompanionForm";
 
 const NewCompanion = () => {
   return (
-    <div>NewCompanion</div>
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      <article>
+        <h1>Companion Builder</h1>
+
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
