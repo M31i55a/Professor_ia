4aa342624d876f52013efe6ac6b8de646aae6777
M31i55a <melysgioda34@gmail.com>
2026-04-12 14:51:31 +0100

    boiler plate

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const Page = () => {
   return (
     <div>
       <h1 className='text-2xl underline'>Hello and welcome to the app</h1>
+      <Button>Let's get started!!!</Button>
     </div>
   )
 }
