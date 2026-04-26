ab759fd1bbe04016be30b6a29ee8447a3e03427c
M31i55a <melysgioda34@gmail.com>
2026-04-26 15:34:09 +0000

    CTA section updated'

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -9,13 +9,13 @@ const Cta = ({ className }: { className?: string }) => {
                 Build and Personalize Learning Companion
             </h2>
             <p>Pick a name, subject, voice, & personality — and start learning through voice conversations that feel natural and fun.</p>
-            <Image src="images/cta.svg" alt="cta" width={362} height={232} />
-            <button className="btn-primary bg-black">
-                <Image src="/icons/plus.svg" alt="plus" width={12} height={12}/>
+            <button className="cta-btn">
+                <Image src="/icons/plus.svg" alt="plus" width={14} height={14}/>
                 <Link href="/companions/new">
                     <p>Build a New Companion</p>
                 </Link>
             </button>
+            <Image src="images/cta.svg" alt="cta" width={362} height={232} />
         </section>
     )
 }
